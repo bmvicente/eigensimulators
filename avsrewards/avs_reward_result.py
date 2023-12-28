198536c36a9b1fe0967660aef541c506a4b534bb
Bernardo Vicente <bernardov.vicente@gmail.com>
2023-12-28 17:20:31 +0000

    update

--- avsrewards/avs_reward_result.py
+++ avsrewards/avs_reward_result.py
@@ -6,12 +6,12 @@ from avs_dual_staking import dual_staking
 from avs_reward_calculation_logic import avs_rewards
 from avs_revenue import revenue
 from avs_tokenomics import tokenomics
-from avs_tvl_totalstaked import tvl_total_staked
+from avs_tvl_totalstaked import tvl_total_staked, avs_total_staked
 from avs_type import type
 
 
-def calculate_rewards(avs_revenue, avs_tvl, avs_total_staked, avs_token_percentage, xeth_percentage, avs_type, security_audits):
-    staker_reward, operator_reward = avs_rewards(avs_revenue, avs_tvl, avs_total_staked, avs_token_percentage, xeth_percentage, avs_type, security_audits)
+def calculate_rewards():
+    staker_reward, operator_reward = avs_rewards(revenue, tvl_total_staked, avs_total_staked, dual_staking, type, avs_sec_audits)
 
     if avs_total_staked != 0:
             staker_reward_percentage = (staker_reward / avs_total_staked) * 100
