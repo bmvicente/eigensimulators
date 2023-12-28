794cfa37ddf7ec869ce433c18f7adf195ef8cbc7
Bernardo Vicente <bernardov.vicente@gmail.com>
2023-12-28 16:26:59 +0000

    update

--- avsrewards/avs_reward_result.py
+++ avsrewards/avs_reward_result.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 from avs_audits import avs_sec_audits
 from avs_dual_staking import dual_staking
-from avs_reward_calculation_logic import avs_rewards, avs_revenue, avs_tvl, avs_total_staked, avs_token_percentage, xeth_percentage, avs_type, security_audits
+from avs_reward_calculation_logic import avs_rewards
 from avs_revenue import revenue
 from avs_tokenomics import tokenomics
 from avs_tvl_totalstaked import tvl_total_staked
