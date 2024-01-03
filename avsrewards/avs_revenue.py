496dabf05b18687a47a2b630a6666562d9aca644
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-01-03 21:33:37 +0000

    update

--- avsrewards/avs_revenue.py
+++ avsrewards/avs_revenue.py
@@ -24,7 +24,7 @@ def avs_revenue_main():
     st.markdown('<p class="header-style">AVS Revenue ($)</p>', unsafe_allow_html=True)
 
     def get_avs_revenue():
-        return st.number_input(".", min_value=0, max_value=1000000000000, value=0, step=1000000)
+        return st.number_input("", min_value=0, max_value=1000000000000, value=0, step=1000000)
 
     avs_revenue_value = get_avs_revenue()
 
