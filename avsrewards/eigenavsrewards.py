b5f7b0fd01bc993123f85a78879338696f24dc07
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-01-05 16:11:01 +0000

    update

--- avsrewards/eigenavsrewards.py
+++ avsrewards/eigenavsrewards.py
@@ -137,9 +137,9 @@ def main():
         st.markdown("""
                 A couple of assumptions were made in our Simulator to simplify and more easily illustrate the potential reward emission of an AVS in a ***Dual Staking*** and ***LST Restaking*** context:
 
-                **Dual Staking** was chosen as the business model, preferred over other models, due to the utility and security they confer to an AVS, and **Veto Dual Staking**, as the Dual Staking method, preferred over other methods, due to the low implementation cost and reliability in terms of liveness. [Learn more](https://www.blog.eigenlayer.xyz/dual-staking/)
+                - **Dual Staking** was chosen as the business model, preferred over other models, due to the utility and security they confer to an AVS, and **Veto Dual Staking**, as the Dual Staking method, preferred over other methods, due to the low implementation cost and reliability in terms of liveness. [Learn more](https://www.blog.eigenlayer.xyz/dual-staking/)
 
-                **LST Restaking** was chosen as the Restaking Modality because the Dual Staking model requires a staked ETH of some kind, and also because it was simpler and more intuitive, compared to other modalities. [Learn more](https://docs.eigenlayer.xyz/overview/readme/whitepaper)
+                - **LST Restaking** was chosen as the Restaking Modality because the Dual Staking model requires a staked ETH of some kind, and also because it was simpler and more intuitive, compared to other modalities. [Learn more](https://docs.eigenlayer.xyz/overview/readme/whitepaper)
 
                 Generally speaking, these three assumptions were made due to the safety they confer AVSs, especially new-born ones, and for simplicity's sake in a first version of the Simulator.
                     
