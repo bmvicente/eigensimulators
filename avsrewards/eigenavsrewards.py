4afddfdd9cb011eace086eb848c7cee40e1fc727
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-01-05 00:27:21 +0000

    update

--- avsrewards/eigenavsrewards.py
+++ avsrewards/eigenavsrewards.py
@@ -508,7 +508,7 @@ def main():
                 ***Operator Reward*** is naturally being given greater weight than the ***Staker Reward*** due to their paramount role in validating modules crucial to the well-function of an AVS.
 
                 The \$AVS’s Tokenomics (while not included in the reward calculation) were deemed useful to include since they provide a look-ahead perspective of how the native AVS token can influence future rewards. A potential for improved rewards to be emitted in the future exists if a relatively small delta between circulating and total supply and a deflationary token rate exist. Whereas a larger delta and an inflationary token rate indicate the potential for lower rewards to be emitted in the future. 
-                An important factor that should determine the $AVS minting rate is that of rewarding operators for their capital costs.
+                An important factor that should help AVS developers determine the $AVS minting rate is that of rewarding operators for their capital costs.
 
                 For a deeper dive, please visit the [source code](https://github.com/bmvicente/eigensimulators/blob/master/avsrewards/eigenavsrewards.py).
             """)
