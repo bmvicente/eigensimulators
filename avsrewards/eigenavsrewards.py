6cf9bb862a5824d79901168050be9ac29000bd51
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-01-05 21:19:19 +0000

    update

--- avsrewards/eigenavsrewards.py
+++ avsrewards/eigenavsrewards.py
@@ -247,7 +247,7 @@ def main():
                     - The **Current TVL** equals **\${avs_tvl:,}**.
                     - The **Current Total Staked (\$AVS & xETH)** equals **\${avs_total_staked:,}**;
 
-                    Accordingly, the main goal is to maintain the *CfC (Cost from Corruption) above the PfC (Profit from Corruption)* to desincentivize colluding operators to perform an attack.
+                    Accordingly, the main goal is to maintain the *CfC (Cost from Corruption)* ***above*** *the PfC (Profit from Corruption)* to desincentivize colluding operators to perform an attack.
 
                     In a Dual Staking Model and Veto Dual Staking context such as ours increasing the [CVS (Cost to Violate Safety) and the CVL (Cost to Violate Liveness)](https://www.blog.eigenlayer.xyz/dual-staking/), calculated as ⅔ \$AVS + ⅔ xETH and ⅓ \$AVS respectively, is important to maintain the health of the AVS dual token pool (or AVS TVL, in other words).
                 """)
