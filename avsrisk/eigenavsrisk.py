f2fe135b7f4d3c4f694e50efbce06ba473520961
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-01-26 19:00:27 +0100

    update

--- avsrisk/eigenavsrisk.py
+++ avsrisk/eigenavsrisk.py
@@ -113,23 +113,21 @@ def main():
                 tvl = st.number_input("**AVS TVL ($)**", min_value=0, max_value=10000000000, value=0, step=1000000)
                 st.write(f"&#8226; AVS TVL: ${tvl:,.0f}")
 
-                st.write("\n")
-
-                st.write("**PARAMETER WEIGHTING**", help="Accounts for Likelihood of the parameter imposing a risk and the Impact that risk would have in the ecosystem.")
-
-                tvl_total_restaked_likelihood = st.slider("**Likelihood**", min_value=1, max_value=10, value=5,
-                                                          help="1 == Unlikely | 10 == Very Likely")
-
         with col4:
                 # Manual input for Total Restaked on AVS
                 total_restaked = st.number_input("**AVS Total Restaked ($)**", min_value=0, max_value=10000000000, value=0, step=1000000)
                 st.write(f"&#8226; AVS Total Restaked: ${total_restaked:,.0f}")
-                st.write("\n")
-                st.write("\n")
-                st.write("\n")
+        
+        st.write("**PARAMETER WEIGHTING**", help="Accounts for Likelihood of the parameter imposing a risk and the Impact that risk would have in the ecosystem.")
 
-                tvl_total_restaked_impact = st.slider("**Impact**", min_value=1, max_value=10, value=5, 
+        col20,col21 = st.columns([3, 3])
+        with col20:
+            tvl_total_restaked_likelihood = st.slider("**Likelihood**", min_value=1, max_value=10, value=5,
+                                                          help="1 == Unlikely | 10 == Very Likely")
+        with col21:
+            tvl_total_restaked_impact = st.slider("**Impact**", min_value=1, max_value=10, value=5, 
                                                       help="1 == Unimpactful | 10 == Very Impactful")
+
 
         # Convert input strings to float for calculation
         tvl = float(tvl) if tvl else 0
