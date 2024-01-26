3b436fc0210a7ddddb38022ac32f4577ef3832cd
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-01-26 14:56:55 +0100

    “update”

--- eigen_cryptoeconomic_risk/cral_staker_collateralization.py
+++ eigen_cryptoeconomic_risk/cral_staker_collateralization.py
@@ -281,6 +281,7 @@ def main():
     st.write("\n")
     st.write("\n")
     st.write("\n")
+    st.write("\n")
 
     st.write(f"""
             **Suggestions on How to Fix *Undercollateralization*:**
