38d53feff9e6dfcf021ab61c1634bfe376884389
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-01-27 23:15:23 +0100

    update

--- eigen_cryptoeconomic_risk/crall_slashing_event.py
+++ eigen_cryptoeconomic_risk/crall_slashing_event.py
@@ -327,8 +327,13 @@ def main():
                     border-radius: 2px;
                     padding: 5px;
                     text-align: center;
-                    margin: 5px 0;">
-                    <h2 style="color: black; margin:0; font-size: 1em;">Own Amount Staked on <i>AVS 2</i>: <span style="font-size: 1.1em;">{stake_avs_2:,.0f}</span></h2>
+                    margin: 5px 0;
+                    background-color: white;">
+                    <h2 style="color: black; margin: 0; font-size: 1.1em;">
+                        <div style="display: block;">
+                            <span style="font-weight: bold; font-size: 1em;">γ<sub style="font-size: 0.9em;">i AVS2</sub></span> | Own Amount Staked on <i>AVS 1</i>: <span style="font-size: 1.1em;">{stake_avs_2:,.0f}</span>
+                        </div>
+                    </h2>
                 </div>
                 """, 
                 unsafe_allow_html=True
@@ -342,8 +347,13 @@ def main():
                     border-radius: 2px;
                     padding: 5px;
                     text-align: center;
-                    margin: 5px 0;">
-                    <h2 style="color: black; margin:0; font-size: 1em;">Own Amount Staked on <i>AVS 3</i>: <span style="font-size: 1.1em;">{stake_avs_3:,.0f}</span></h2>
+                    margin: 5px 0;
+                    background-color: white;">
+                    <h2 style="color: black; margin: 0; font-size: 1.1em;">
+                        <div style="display: block;">
+                            <span style="font-weight: bold; font-size: 1em;">γ<sub style="font-size: 0.9em;">i AVS3</sub></span> | Own Amount Staked on <i>AVS 1</i>: <span style="font-size: 1.1em;">{stake_avs_3:,.0f}</span>
+                        </div>
+                    </h2>
                 </div>
                 """, 
                 unsafe_allow_html=True
