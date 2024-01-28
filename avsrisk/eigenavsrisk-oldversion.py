b325f2e196aee4194d9abadd568efde22fc054ef
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-01-29 00:18:26 +0100

    “update”

--- avsrisk/eigenavsrisk-oldversion.py
+++ avsrisk/eigenavsrisk-oldversion.py
@@ -358,7 +358,7 @@ def main():
         text-align: center;
         margin: 10px 0;
         background-color: {background_color};">
-        <h2 style="color: black; margin:0; font-size: 1.4em;">Normalized AVS Risk Score: <span style="font-size: 1.5em; color: {color};">{risk_score:.0f}</span></h2>
+        <h2 style="color: black; margin:0; font-size: 1.4em;">AVS Risk Score: <span style="font-size: 1.5em; color: {color};">{risk_score:.0f}</span></h2>
     </div>
     """, 
     unsafe_allow_html=True
