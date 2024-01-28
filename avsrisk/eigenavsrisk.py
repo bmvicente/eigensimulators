18e8eb0ad89ca7770a1942f27a93a10cb37a637b
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-01-28 15:29:10 +0100

    “update”

--- avsrisk/eigenavsrisk.py
+++ avsrisk/eigenavsrisk.py
@@ -182,7 +182,7 @@ def main():
                 <span style="font-size: 24px; font-weight: bold;"> = </span>
                 <span style="font-size: 22px; font-weight: bold; background-color: #7FCAE5; border-radius: 10px; padding: 5px; margin: 2px;">{result1}</span>
                 <div style="text-align: center; margin-top: 10px;">
-            <span style="font-size: 14px; font-weight: bold;">(Parameter Risk based on Input * Likelihood * Impact = Weighted Overall Risk for Parameter)</span>
+            <span style="font-size: 16px; font-weight: bold;">(Parameter Risk based on Input * Likelihood * Impact = Weighted Overall Risk for Parameter)</span>
         </div>
         """
 
