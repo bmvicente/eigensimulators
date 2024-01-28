2f0cc65a97fe796994dc5c635b01154e84db77d4
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-01-28 02:19:11 +0100

    update

--- eigen_cryptoeconomic_risk/crall_slashing_event.py
+++ eigen_cryptoeconomic_risk/crall_slashing_event.py
@@ -1202,7 +1202,7 @@ def main():
                 <div style="color: black; margin:0; font-size: 1.5em; font-weight: bold;">
                     AVS 3
                 </div>
-                <div style="color: black; font-size: 1.2em; margin-top: 10px; font-weight: bold;">
+                <div style="color: black; font-size: 1.2em; margin-top: 5px; font-weight: bold;">
                     ${max_slash_allowed3:,.0f} - ${op_max_loss_avs3:,.0f} = <span style="font-size: 1.3em; color: {color};">${bst_avs3:,.0f}</span>
                 </div>
             </div>
