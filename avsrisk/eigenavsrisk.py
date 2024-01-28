9626156fe9a3e5154795602997fd90d3931c023d
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-01-28 22:56:51 +0100

    “update”

--- avsrisk/eigenavsrisk.py
+++ avsrisk/eigenavsrisk.py
@@ -521,7 +521,7 @@ def main():
     st.write("  \n")
     st.write("  \n")
     
-    st.session_state.risk_score, st.session_state.security_audit_score, st.session_state.business_model_score, st.session_state.avs_type_score, st.session_state.restaking_mod_score, st.session_state.avs_avg_operator_reputation_score = avs_risk(security_audits, business_model, avs_type, operator_attack_risk, restaking_mods, avs_avg_operator_reputation)
+    risk_score, security_audit_score, business_model_score, avs_type_score, restaking_mod_score, avs_avg_operator_reputation_score = avs_risk(security_audits, business_model, avs_type, operator_attack_risk, restaking_mods, avs_avg_operator_reputation)
 
 
     # Determine the color and background color based on the risk score
