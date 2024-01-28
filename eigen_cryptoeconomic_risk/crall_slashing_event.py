63dab99bc4f38edbf2392adf2ece81e403ea9159
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-01-28 01:28:21 +0100

    update

--- eigen_cryptoeconomic_risk/crall_slashing_event.py
+++ eigen_cryptoeconomic_risk/crall_slashing_event.py
@@ -1105,7 +1105,7 @@ def main():
     st.write("\n")
 
     st.write(f"""
-             The BST test helps identify the ecosystem element(s) that are in a compromisable state due to a previous-executed Operator slashing event.
+             The Byzantine *Slashing* Tolerance test helps identify the ecosystem element(s) that are in a compromisable state due to a previous-executed Operator slashing event.
              
              We say that an element has failed the BST test if B < 0, and passed if B > 0.
             
