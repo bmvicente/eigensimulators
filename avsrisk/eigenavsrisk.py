c45b8a0fca18def7d34e7e695e387ab8c56e3954
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-01-29 16:43:53 +0100

    “update”

--- avsrisk/eigenavsrisk.py
+++ avsrisk/eigenavsrisk.py
@@ -146,11 +146,11 @@ def main():
         # Display the result with formatting
         tvl_total_restaked_calc = f"""
             <div style="text-align: center;">
-                <span style="font-size: 22px; font-weight: bold; background-color: grey; border-radius: 10px; padding: 5px; margin: 2px;">{st.session_state.operator_attack_risk}</span> 
+                <span style="font-size: 22px; font-weight: bold; background-color: lightgrey; border-radius: 10px; padding: 5px; margin: 2px;">{st.session_state.operator_attack_risk}</span> 
                 <span style="font-size: 24px; font-weight: bold;">&times;</span>
-                <span style="font-size: 22px; font-weight: bold; background-color: lightgreen; border-radius: 10px; padding: 5px; margin: 2px;">{tvl_total_restaked_likelihood}</span> 
+                <span style="font-size: 22px; font-weight: bold; background-color: green; border-radius: 10px; padding: 5px; margin: 2px;">{tvl_total_restaked_likelihood}</span> 
                 <span style="font-size: 24px; font-weight: bold;">&times;</span>
-                <span style="font-size: 22px; font-weight: bold; background-color: green; border-radius: 10px; padding: 5px; margin: 2px;">{tvl_total_restaked_impact}</span> 
+                <span style="font-size: 22px; font-weight: bold; background-color: lightgreen; border-radius: 10px; padding: 5px; margin: 2px;">{tvl_total_restaked_impact}</span> 
                 <span style="font-size: 24px; font-weight: bold;"> = </span>
                 <span style="font-size: 22px; font-weight: bold; border-radius: 10px; padding: 5px; margin: 2px;">{result1}</span>
                 <div style="text-align: center; margin-top: 10px;">
