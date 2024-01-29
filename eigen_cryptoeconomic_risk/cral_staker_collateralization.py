224c0132d6cb1c7e906d1657c16358817f2983d2
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-01-29 16:06:11 +0100

    “update”

--- eigen_cryptoeconomic_risk/cral_staker_collateralization.py
+++ eigen_cryptoeconomic_risk/cral_staker_collateralization.py
@@ -318,6 +318,7 @@ def main():
     st.write(f"""
             An Operator may be voluntarily or involuntarily undercollateralized. They may have position themselves that way to extract Profit from tasks they're validating or other ecosystem dependencies may have put them in that position and their collateralization status has a non-malicious intent.
             <br>
+            <br>
             **Suggestions on How to Fix *Undercollateralization*:**
 
             1. The Undercollateralized Staker can increase their amount of stake;
