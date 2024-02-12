3649513933f62e6bf1f51d83dfc1873a9e9446cd
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-02-12 23:30:21 +0100

    update

--- eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
+++ eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
@@ -130,6 +130,14 @@ def main():
     if 'post_slash_total_restaked' not in st.session_state:
         st.session_state.post_slash_total_restaked = 0
     
+    if 'st.session_state.avs1_category' not in st.session_state:
+        st.session_state.avs1_category = 0    
+    if 'st.session_state.avs2_category' not in st.session_state:
+        st.session_state.avs2_category = 0    
+    if 'st.session_state.avs3_category' not in st.session_state:
+        st.session_state.avs3_category = 0
+        
+    
 
     st.session_state.pre_slash_coc = st.session_state.pre_slash_total_restaked / 3
 
