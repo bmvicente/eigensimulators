7d1f0ca238b66345cfab7479f3d711f5f6c4e68a
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-02-13 17:07:51 +0100

    update

--- eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
+++ eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
@@ -643,7 +643,7 @@ def main():
                         <div style="
                             border: 1px solid;
                             border-radius: 2px;
-                            padding: 15px;
+                            padding: 5px;
                             text-align: center;
                             margin: 5px 0;
                             background-color: white;">
@@ -679,7 +679,7 @@ def main():
                     <div style="
                         border: 1px solid;
                         border-radius: 2px;
-                        padding: 5px;
+                        padding: 15px;
                         text-align: center;
                         margin: 5px 0;
                         background-color: white;">
