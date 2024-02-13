e5b6343b22d45c10dd3f44da27f17abcad54c699
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-02-13 02:46:19 +0100

    update

--- eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
+++ eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
@@ -420,7 +420,11 @@ def main():
             return 0  # Ensure a numeric return
 
 
-    service_categories_evaluation_result = evaluate_service_categories(st.session_state.avs1_category, st.session_state.avs2_category, st.session_state.avs3_category)
+    evaluate_avs1_category = st.session_state.avs1_category
+    evaluate_avs2_category = st.session_state.avs2_category
+    evaluate_avs3_category = st.session_state.avs3_category
+
+    service_categories_evaluation_result = evaluate_service_categories(evaluate_avs1_category, evaluate_avs2_category, evaluate_avs3_category)
     conditions_evaluation_result = evaluate_conditions(st.session_state.pre_slash_coc, st.session_state.post_slash_coc)
 
     risk_category1 = categorize_risk(st.session_state.risk_score1)
