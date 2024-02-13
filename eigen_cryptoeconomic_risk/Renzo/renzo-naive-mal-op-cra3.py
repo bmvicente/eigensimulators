ecc1669e15ea2c7f5edcf01871e32395273953e6
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-02-14 00:01:11 +0100

    update

--- eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
+++ eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
@@ -1144,6 +1144,14 @@ def main():
         unsafe_allow_html=True
     )
 
+    if background_color == "#ff6666":
+        st.markdown("""
+            <div style="color: red; font-weight: bold; font-size: 20px;">
+                The System Has Been Compromised Due to Lack of Cryptoeconomic Security.
+            </div>
+            """, unsafe_allow_html=True)
+
+
 
     st.write("  \n")
 
