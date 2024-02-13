4dd40b7b9c5130a1c0df923d5c3a8e7fb9634527
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-02-13 03:08:48 +0100

    update

--- eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
+++ eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
@@ -439,74 +439,8 @@ def main():
 
 
 
-    # Creating the markdown string
-    final_result_service_1_calc = f"""
-        <div style="text-align: center;">
-            <span style="font-size: 22px; font-weight: bold; background-color: lightgrey; border-radius: 10px; padding: 5px; margin: 2px;">{risk_evaluation}</span> 
-            <span style="font-size: 24px; font-weight: bold;">&times;</span>
-            <span style="font-size: 22px; font-weight: bold; background-color: lightgreen; border-radius: 10px; padding: 5px; margin: 2px;">{service_categories_evaluation_result}</span> 
-            <span style="font-size: 24px; font-weight: bold;">&times;</span>
-            <span style="font-size: 22px; font-weight: bold; background-color: lightblue; border-radius: 10px; padding: 5px; margin: 2px;">{conditions_evaluation_result}</span> 
-            <span style="font-size: 24px; font-weight: bold;"> = </span>
-            <span style="font-size: 22px; font-weight: bold; border-radius: 10px; padding: 5px; margin: 2px;">{final_result_service_1}</span>
-        </div>
-        """
-
-    st.markdown(final_result_service_1_calc, unsafe_allow_html=True)
-
-
-    # Creating the markdown string
-    final_result_service_2_calc = f"""
-        <div style="text-align: center;">
-            <span style="font-size: 22px; font-weight: bold; background-color: lightgrey; border-radius: 10px; padding: 5px; margin: 2px;">{risk_evaluation}</span> 
-            <span style="font-size: 24px; font-weight: bold;">&times;</span>
-            <span style="font-size: 22px; font-weight: bold; background-color: lightgreen; border-radius: 10px; padding: 5px; margin: 2px;">{service_categories_evaluation_result}</span> 
-            <span style="font-size: 24px; font-weight: bold;">&times;</span>
-            <span style="font-size: 22px; font-weight: bold; background-color: lightblue; border-radius: 10px; padding: 5px; margin: 2px;">{conditions_evaluation_result}</span> 
-            <span style="font-size: 24px; font-weight: bold;"> = </span>
-            <span style="font-size: 22px; font-weight: bold; border-radius: 10px; padding: 5px; margin: 2px;">{final_result_service_1}</span>
-        </div>
-        """
-
-    # Displaying the markdown in Streamlit
-    st.markdown(final_result_service_2_calc, unsafe_allow_html=True)
-
-
-
-    # Creating the markdown string
-    final_result_service_3_calc = f"""
-        <div style="text-align: center;">
-            <span style="font-size: 22px; font-weight: bold; background-color: lightgrey; border-radius: 10px; padding: 5px; margin: 2px;">{risk_evaluation}</span> 
-            <span style="font-size: 24px; font-weight: bold;">&times;</span>
-            <span style="font-size: 22px; font-weight: bold; background-color: lightgreen; border-radius: 10px; padding: 5px; margin: 2px;">{service_categories_evaluation_result}</span> 
-            <span style="font-size: 24px; font-weight: bold;">&times;</span>
-            <span style="font-size: 22px; font-weight: bold; background-color: lightblue; border-radius: 10px; padding: 5px; margin: 2px;">{conditions_evaluation_result}</span> 
-            <span style="font-size: 24px; font-weight: bold;"> = </span>
-            <span style="font-size: 22px; font-weight: bold; border-radius: 10px; padding: 5px; margin: 2px;">{final_result_service_1}</span>
-        </div>
-        """
-
-    # Displaying the markdown in Streamlit
-    st.markdown(final_result_service_3_calc, unsafe_allow_html=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     col1, col2 = st.columns([1, 1], gap="large")
-
-
-
 
 
 
@@ -741,6 +675,21 @@ def main():
             unsafe_allow_html=True
         )
         
+
+        final_result_service_1_calc = f"""
+        <div style="text-align: center;">
+            <span style="font-size: 22px; font-weight: bold; background-color: lightgrey; border-radius: 10px; padding: 5px; margin: 2px;">{risk_evaluation}</span> 
+            <span style="font-size: 24px; font-weight: bold;">&times;</span>
+            <span style="font-size: 22px; font-weight: bold; background-color: lightgreen; border-radius: 10px; padding: 5px; margin: 2px;">{service_categories_evaluation_result}</span> 
+            <span style="font-size: 24px; font-weight: bold;">&times;</span>
+            <span style="font-size: 22px; font-weight: bold; background-color: lightblue; border-radius: 10px; padding: 5px; margin: 2px;">{conditions_evaluation_result}</span> 
+            <span style="font-size: 24px; font-weight: bold;"> = </span>
+            <span style="font-size: 22px; font-weight: bold; border-radius: 10px; padding: 5px; margin: 2px;">{final_result_service_1}</span>
+        </div>
+        """
+
+        st.markdown(final_result_service_1_calc, unsafe_allow_html=True)
+
         st.write("\n")
 
 
@@ -880,6 +829,21 @@ def main():
         )
 
 
+        final_result_service_2_calc = f"""
+        <div style="text-align: center;">
+            <span style="font-size: 22px; font-weight: bold; background-color: lightgrey; border-radius: 10px; padding: 5px; margin: 2px;">{risk_evaluation}</span> 
+            <span style="font-size: 24px; font-weight: bold;">&times;</span>
+            <span style="font-size: 22px; font-weight: bold; background-color: lightgreen; border-radius: 10px; padding: 5px; margin: 2px;">{service_categories_evaluation_result}</span> 
+            <span style="font-size: 24px; font-weight: bold;">&times;</span>
+            <span style="font-size: 22px; font-weight: bold; background-color: lightblue; border-radius: 10px; padding: 5px; margin: 2px;">{conditions_evaluation_result}</span> 
+            <span style="font-size: 24px; font-weight: bold;"> = </span>
+            <span style="font-size: 22px; font-weight: bold; border-radius: 10px; padding: 5px; margin: 2px;">{final_result_service_1}</span>
+        </div>
+        """
+
+        # Displaying the markdown in Streamlit
+        st.markdown(final_result_service_2_calc, unsafe_allow_html=True)
+
         st.write("\n")
 
     
@@ -1017,6 +981,22 @@ def main():
             """, 
             unsafe_allow_html=True
         )
+
+
+        final_result_service_3_calc = f"""
+            <div style="text-align: center;">
+                <span style="font-size: 22px; font-weight: bold; background-color: lightgrey; border-radius: 10px; padding: 5px; margin: 2px;">{risk_evaluation}</span> 
+                <span style="font-size: 24px; font-weight: bold;">&times;</span>
+                <span style="font-size: 22px; font-weight: bold; background-color: lightgreen; border-radius: 10px; padding: 5px; margin: 2px;">{service_categories_evaluation_result}</span> 
+                <span style="font-size: 24px; font-weight: bold;">&times;</span>
+                <span style="font-size: 22px; font-weight: bold; background-color: lightblue; border-radius: 10px; padding: 5px; margin: 2px;">{conditions_evaluation_result}</span> 
+                <span style="font-size: 24px; font-weight: bold;"> = </span>
+                <span style="font-size: 22px; font-weight: bold; border-radius: 10px; padding: 5px; margin: 2px;">{final_result_service_1}</span>
+            </div>
+            """
+
+        # Displaying the markdown in Streamlit
+        st.markdown(final_result_service_3_calc, unsafe_allow_html=True)
 
         st.write("\n")
     
