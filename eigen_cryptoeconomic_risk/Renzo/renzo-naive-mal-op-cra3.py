e274a8234c8ef4169bb05e0ccef50153ca7c607b
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-02-14 16:31:00 +0100

    update

--- eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
+++ eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
@@ -1180,7 +1180,7 @@ def main():
             background-color: {background_color};">
             <h2 style="color: black; margin: 0; font-size: 1.4em;">
                 <div style="display: block; margin-top: 5px;">
-                    <span style="font-size: 1.1em;"><i>STAKESURE</i></span> - Available Insurance Reserve from Operator Slashed Funds <span style="font-size: 0.8em; font-weight: normal;">(Tt+1 / 2)</span>: <span style="font-size: 1.1em;">${stakesure_insurance_reserve:,.0f}</span>
+                    <span style="font-size: 1.1em;"><i>STAKESURE</i></span> - Available Insurance Reserve from Operator Slashed Funds: <span style="font-size: 1.1em;">${stakesure_insurance_reserve:,.0f}</span>
                     <br><span style="font-size: 18px; font-weight: bold;">{message}</span>
                 </div>
             </h2>
