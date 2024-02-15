85189bcecca3f67fef9ec5892efd2825f5aaa03c
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-02-15 03:35:39 +0100

    update

--- eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
+++ eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
@@ -1407,7 +1407,7 @@ def main():
             message1 = "No Insurance Needed from Buffer"
             buffer1 = 0
         elif st.session_state.insurance_statuses['avs1_insurance_status'] == insurance_options[1]:  # Bought inappropriate amount
-            percentage_uninsured_1 = st.slider("% Amount Insured for AVS1", 0, 100, 50, key='percentage_uninsured_1') / 100
+            percentage_uninsured_1 = st.slider("% Amount Uninsured for AVS1", 0, 100, 50, key='percentage_uninsured_1') / 100
             buffer1 = final_result_service_1 * percentage_uninsured_1
             message1 = f"Buffer Insurance Amount Needed: ${buffer1:,.0f}"
         else:  # Didn't buy insurance
@@ -1420,7 +1420,7 @@ def main():
             message2 = "No Insurance Needed from Buffer"
             buffer2 = 0
         elif st.session_state.insurance_statuses['avs2_insurance_status'] == insurance_options[1]:  # Bought inappropriate amount
-            percentage_uninsured_2 = st.slider("% Amount Insured for AVS2", 0, 100, 50, key='percentage_uninsured_2') / 100
+            percentage_uninsured_2 = st.slider("% Amount Uninsured for AVS2", 0, 100, 50, key='percentage_uninsured_2') / 100
             buffer2 = final_result_service_2 * percentage_uninsured_2
             message2 = f"Buffer Insurance Amount Needed: ${buffer2:,.0f}"
         else:  # Didn't buy insurance
@@ -1433,7 +1433,7 @@ def main():
             message3 = "No Insurance Needed from Buffer"
             buffer3 = 0
         elif st.session_state.insurance_statuses['avs3_insurance_status'] == insurance_options[1]:  # Bought inappropriate amount
-            percentage_uninsured_3 = st.slider("% Amount Insured for AVS3", 0, 100, 50, key='percentage_uninsured_3') / 100
+            percentage_uninsured_3 = st.slider("% Amount Uninsured for AVS3", 0, 100, 50, key='percentage_uninsured_3') / 100
             buffer3 = final_result_service_3 * percentage_uninsured_3
             message3 = f"Buffer Insurance Amount Needed: ${buffer3:,.0f}"
         else:  # Didn't buy insurance
