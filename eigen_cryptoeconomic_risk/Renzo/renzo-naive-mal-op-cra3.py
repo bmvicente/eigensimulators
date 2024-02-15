b6bc8d7aa1d475330117fb453e697d60d3c12c94
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-02-15 02:37:58 +0100

    update

--- eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
+++ eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
@@ -1519,7 +1519,7 @@ def main():
 
                 
     # Adjust the column widths to try and center the button more effectively
-    col1, col2, col3 = st.columns([1,10,1])
+    col1, col2, col3 = st.columns([1,1,10])
 
     # Use the middle column for the button
     with col2:
