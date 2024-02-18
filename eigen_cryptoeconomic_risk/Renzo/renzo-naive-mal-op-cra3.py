181a61834006f11ac43ce5ef8fab746bbe5be709
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-02-18 15:00:08 +0100

    update

--- eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
+++ eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
@@ -262,7 +262,8 @@ def main():
                                 <div style="display: block;">
                                     <span style="font-size: 1.2em;">α<sub style="font-size: 0.8em;">jt</sub></span> &nbsp; | &nbsp;
                                     {max_slash_allowed_text}: <span style="font-size: 1.1em;">${abs(pre_slash_max_slash_allowed):,.0f}</span>
-                                    <span style="font-size: 16px; font-weight: normal;">(CoC t - PfC t)</span>
+                                    <br>
+                                    <span style="font-size: 16px; font-weight: bold;">(CoC t - PfC t)</span>
                                 </div>
                             </h2>
                         </div>
@@ -355,7 +356,8 @@ def main():
                     <div style="display: block;">
                         <span style="font-size: 1.2em;">δ<sub style="font-size: 0.8em;">ijt+1</sub></span> &nbsp; | &nbsp;
                         Actual Slash on Cryptoeconomic Security: <span style="font-size: 1.1em;">${actual_slash_on_cs:,.0f}</span>
-                        <span style="font-size: 16px; font-weight: normal;">(since PfC remains unchanged post-slash, the de facto cryptoeconomic slash = CoC t - CoC t+1. More on Logic below.)</span>
+                        <br>
+                        <span style="font-size: 16px; font-weight: bold;">(since PfC remains unchanged post-slash, the de facto cryptoeconomic slash = CoC t - CoC t+1. More on Logic below.)</span>
                     </div>
                 </h2>
             </div>
