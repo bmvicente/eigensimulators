ba518300fdb963f2a6c8d2bf4e6e142c64acfede
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-02-18 14:49:57 +0100

    update

--- eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
+++ eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
@@ -345,7 +345,7 @@ def main():
                 unsafe_allow_html=True
             )
 
-        st.markdown(
+            st.markdown(
                 f"""
                 <div style="
                     border: 2px solid;
@@ -359,8 +359,8 @@ def main():
                             <span style="font-size: 1.2em;">δ<sub style="font-size: 0.8em;">ijt+1</sub></span> &nbsp; | &nbsp;
                             Actual Slash on Cryptoeconomic Security: <span style="font-size: 1.1em;">${actual_slash_on_cs:,.0f}</span>
                         </div>
-                        <!-- Correctly placed outside of <h2> and with normal font weight -->
-                        <div style="font-size: 16px; margin-top: 5px;">
+                        <!-- Explicitly ensuring normal font weight -->
+                        <div style="font-size: 16px; margin-top: 5px; font-weight: normal;">
                             (since PfC remains unchanged post-slash, the de facto cryptoeconomic slash = CoC t - CoC t+1. More on Logic below.)
                         </div>
                     </div>
@@ -368,6 +368,7 @@ def main():
                 """, 
                 unsafe_allow_html=True
             )
+
 
 
             
