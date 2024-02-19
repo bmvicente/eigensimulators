7194af94fc5779a206bffc4095809c6afd457fb5
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-02-19 18:59:34 +0100

    update

--- eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
+++ eigen_cryptoeconomic_risk/Renzo/renzo-naive-mal-op-cra3.py
@@ -1641,7 +1641,7 @@ def main():
 
     st.markdown(f"""
             <div style="font-size: 22px;">
-                <b>• Cryptoeconomic Buffer Available for Poorly Insured or Uninsured Users
+                <b>•  Cryptoeconomic Buffer Available for Poorly Insured or Uninsured Users
             </div>
             """, unsafe_allow_html=True)
     
