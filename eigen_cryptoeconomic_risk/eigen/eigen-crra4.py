a4408d5a08f0f8f0adad496e2c6fd1e52ea09cf2
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-07 20:38:59 +0100

    update

--- eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
+++ eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
@@ -1975,7 +1975,8 @@ def main():
                 # Displaying the custom styled header
         st.markdown('<p class="header-style">AVS1</p>', unsafe_allow_html=True)
 
-            # Manual input for AVS TVL
+        st.write("\n")
+
         avs1_revenue = st.number_input("AVS1 Revenue", min_value=0, max_value=1000000000000, value=0, step=1000000, key="avs1_revenue")
         
         st.write(f"""&#8226; AVS1 Revenue: **${avs1_revenue:,.0f}**""")
@@ -2054,7 +2055,8 @@ def main():
                 # Displaying the custom styled header
         st.markdown('<p class="header-style">AVS2</p>', unsafe_allow_html=True)
 
-            # Manual input for AVS TVL
+        st.write("\n")
+
         avs2_revenue = st.number_input("AVS2 Revenue", min_value=0, max_value=1000000000000, value=0, step=1000000, key="avs2_revenue")
 
         st.write(f"""&#8226; AVS2 Revenue: **${avs2_revenue:,.0f}**""")
@@ -2131,7 +2133,8 @@ def main():
                 # Displaying the custom styled header
         st.markdown('<p class="header-style">AVS3</p>', unsafe_allow_html=True)
 
-            # Manual input for AVS TVL
+        st.write("\n")
+
         avs3_revenue = st.number_input("AVS3 Revenue", min_value=0, max_value=1000000000000, value=0, step=1000000, key="avs3_revenue")
 
         st.write(f"""&#8226; AVS3 Revenue: **${avs3_revenue:,.0f}**""")
