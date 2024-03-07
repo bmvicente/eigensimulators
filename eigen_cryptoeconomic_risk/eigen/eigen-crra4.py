ce0347fed120d3b4f87fbdce80be5fbe3ba189dc
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-07 18:36:28 +0100

    update

--- eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
+++ eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
@@ -1916,11 +1916,11 @@ def main():
 
     fraction_html = """
     <div style="text-align: center;">
-        <span style="font-size: 20px; font-weight: bold;">Generic AVS Sharpe Ratio Calculation = </span>
+        <span style="font-size: 20px; font-weight: bold;">Generic AVS Sharpe Ratio Calculation =  </span>
         <div style="display: inline-block; vertical-align: middle; font-size: 20px; font-weight: bold; text-align: center;">
-            <span>AVS Net Profit  -  Min Expected Slash</span><br>
+            <span>AVS Net Profit  -  Min Expected Slash on Co-Joint AVS</span><br>
             <hr style="margin: 2px 0; width: 100%; border-top: 2px solid black;">
-            <span>Excess Return (20%)</span>
+            <span>Excess Return Deviation(20%)</span>
         </div>
     </div> """
 
