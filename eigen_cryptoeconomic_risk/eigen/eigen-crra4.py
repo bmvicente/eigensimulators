72cc7e2e494d9442948ef5b424a45f456a0495fd
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-07 22:03:40 +0100

    update

--- eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
+++ eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
@@ -1925,7 +1925,6 @@ def main():
     st.header("**2. REWARD**")
     st.subheader("**2.1 Staker/Operator Revenue Distributions**")
     
-    st.write("\n")
 
     with st.expander("How this Simulator Works & Basic Assumptions"):
         st.markdown("""
