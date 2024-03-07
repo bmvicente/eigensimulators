330cfc93b9c6f8837cf32b6f4fc1b5aec516c57d
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-07 18:10:36 +0100

    update

--- eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
+++ eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
@@ -77,13 +77,20 @@ def main():
     st.header("**Malicious Operator → AVS Slashing Event Simulator:** *Naïve & STAKESURE Approaches*")
 
     with st.expander("Index"):
-        st.markdown("""1. RISK
-                            1.1 **Malicious Operator → AVS Slashing Event Simulator:** *Naïve Approach*
-                            1.2 **Malicious Operator → AVS Slashing Event Simulator:** *STAKESURE Approach*
-                    2. REWARD
-                            2.1 Staker/Operator Reward Distribution
-                            2.2 Sharpe Ratios
-                    """)
+        st.markdown("""
+    **RISK**
+
+    1. **Malicious Operator → AVS Slashing Event Simulator:** *Naïve Approach*
+
+    2. **Malicious Operator → AVS Slashing Event Simulator:** *STAKESURE Approach*
+
+    **REWARD**
+
+    1. Staker/Operator Reward Distribution
+
+    2. Sharpe Ratios
+        """, unsafe_allow_html=False)
+
 
     st.write("  \n")
 
