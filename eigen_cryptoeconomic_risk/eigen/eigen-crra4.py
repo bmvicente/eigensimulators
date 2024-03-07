6d3c6135f3e0fff49fa9399e7a8c4a0418ea706b
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-07 20:39:56 +0100

    update

--- eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
+++ eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
@@ -1977,7 +1977,7 @@ def main():
 
         st.write("\n")
 
-        avs1_revenue = st.number_input("AVS1 Revenue", min_value=0, max_value=1000000000000, value=0, step=1000000, key="avs1_revenue")
+        avs1_revenue = st.number_input("Revenue", min_value=0, max_value=1000000000000, value=0, step=1000000, key="avs1_revenue")
         
         st.write(f"""&#8226; AVS1 Revenue: **${avs1_revenue:,.0f}**""")
 
@@ -2057,7 +2057,7 @@ def main():
 
         st.write("\n")
 
-        avs2_revenue = st.number_input("AVS2 Revenue", min_value=0, max_value=1000000000000, value=0, step=1000000, key="avs2_revenue")
+        avs2_revenue = st.number_input("Revenue", min_value=0, max_value=1000000000000, value=0, step=1000000, key="avs2_revenue")
 
         st.write(f"""&#8226; AVS2 Revenue: **${avs2_revenue:,.0f}**""")
 
@@ -2135,7 +2135,7 @@ def main():
 
         st.write("\n")
 
-        avs3_revenue = st.number_input("AVS3 Revenue", min_value=0, max_value=1000000000000, value=0, step=1000000, key="avs3_revenue")
+        avs3_revenue = st.number_input("Revenue", min_value=0, max_value=1000000000000, value=0, step=1000000, key="avs3_revenue")
 
         st.write(f"""&#8226; AVS3 Revenue: **${avs3_revenue:,.0f}**""")
 
