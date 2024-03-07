d18041ef8d1decd82d9d8ad811e2257dae9a9f34
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-07 21:09:37 +0100

    update

--- eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
+++ eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
@@ -2162,7 +2162,7 @@ def main():
                     text-align: center;
                     margin: 10px 0;
                     background-color: white;">
-                    <h2 style="color: black; margin:0; font-size: 1em;">$AVS3 Staker / xETH Restaker Reward: <span style="font-size: 1.2em;">m</span></h2>
+                    <h2 style="color: black; margin:0; font-size: 1.1em;">$AVS3 Staker / xETH Restaker Reward: <span style="font-size: 1.2em;">m</span></h2>
                 </div>
                 """, 
                 unsafe_allow_html=True
