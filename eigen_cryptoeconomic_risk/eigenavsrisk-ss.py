37ac70b3a9d0a993d362e677e4d8d2b679a7354b
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-08 16:43:42 +0100

    update

--- eigen_cryptoeconomic_risk/eigenavsrisk-ss.py
+++ eigen_cryptoeconomic_risk/eigenavsrisk-ss.py
@@ -891,6 +891,8 @@ def main():
         st.write("  \n")
         st.write("  \n")
         st.write("  \n")
+        st.write("  \n")
+        st.write("  \n")
 
         custom_css = """
             <style>
@@ -917,7 +919,7 @@ def main():
                 text-align: center;
                 margin: 5px 0;
                 background-color: #CC8E00;"> <!-- Green background color -->
-                <h2 class='large-header-style' style="color: white; margin:0;">OPERATOR METRICS</h2> <!-- Larger font for AVSs -->
+                <h2 class='large-header-style' style="color: white; margin:0;">DELEGATED OPERATOR METRICS</h2> <!-- Larger font for AVSs -->
             </div>
             """, 
             unsafe_allow_html=True
