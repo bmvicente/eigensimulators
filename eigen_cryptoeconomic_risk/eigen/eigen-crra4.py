7f345be124f0da4c529c24d5a603dbe9d0291fc0
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-09 00:10:26 +0100

    update

--- eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
+++ eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
@@ -2908,18 +2908,30 @@ def main():
 
         fraction_html11 = f"""
             <div style="text-align: center;">
-                <div style="display: inline-block; vertical-align: middle; font-size: 21px; font-weight: bold; text-align: center;">
-                    <span>${avs1_net_yield:,.0f} - ${avs1_expected_slash:,.0f}</span><br>
+                <span style="font-size: 21px; font-weight: bold;">In-Isolation Ratio:</span>
+                <div style="display: inline-block; vertical-align: middle; text-align: center;">
+                    <span style="font-size: 21px; font-weight: bold;">${avs1_net_yield:,.0f} - ${avs1_expected_slash:,.0f}</span><br>
                     <hr style="margin: 2px 0; width: 100%; border-top: 2px solid black;">
-                    <span>${avs1_st_dev_abs:,.0f}</span>
+                    <span style="font-size: 21px; font-weight: bold;">${avs1_st_dev_abs:,.0f}</span>
                 </div>
-                <span style="font-size: 25px; font-weight: bold;"> = {sharpe_ratio1:.2f}</span>
+                <span style="font-size: 21px; font-weight: bold;">= {sharpe_ratio1:.2f}</span>
             </div>
         """
 
         st.markdown(fraction_html11, unsafe_allow_html=True)
 
+        fraction_html111 = f"""
+            <div style="text-align: center;">
+                <span style="font-size: 21px; font-weight: bold;">Ecosystem-Aware Ratio:</span>
+                <div style="display: inline-block; vertical-align: middle; text-align: center;">
+                    <span style="font-size: 21px; font-weight: bold;">{sharpe_ratio1:.2f} - ${avs1_net_yield:,.0f} - ${avs1_expected_slash:,.0f}</span><br>
+                    <hr style="margin: 2px 0; width: 100%; border-top: 2px solid black;">
+                </div>
+                <span style="font-size: 21px; font-weight: bold;">= {sharpe_ratio1}</span> <!-- replace with actual resulting value -->
+            </div>
+        """
 
+        st.markdown(fraction_html111, unsafe_allow_html=True)
 
 
 
