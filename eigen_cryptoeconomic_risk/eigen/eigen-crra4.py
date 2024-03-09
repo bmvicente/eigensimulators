031c2c5e7e6f7d286f3689c87ecd8dd1a0db3836
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-09 16:59:15 +0100

    update

--- eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
+++ eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
@@ -2788,6 +2788,13 @@ def main():
 
         with st.expander("Logic"):
             st.markdown("""
+    **AVS Reward Emission:**
+
+    The AVS Reward Emission simulator percentage-results suggest how much should be distributed among $AVS Stakers, xETH Restakers, and AVS Operators, given the risk and economic security profile of the AVS. To build on the Assumptions explanation on top, this calculation is based on the correlated reward-to-risk sensitivity per variable input -- the Logic below each input parameter helps understand the underlying rationale.
+
+    Operator Reward is naturally being given greater weight than the Staker Reward due to their paramount role in validating modules crucial to the well-function of an AVS. We've assigned 60% of distributable rewards to Operators and 40% to Stakers and Restakers.
+
+    The $AVS’s Tokenomics (while not included in the reward calculation) were deemed useful to include since they provide a look-ahead perspective of how the native AVS token can influence future rewards. A potential for improved rewards to be emitted in the future exists if a relatively small delta between circulating and total supply and a deflationary token rate exist. Whereas a larger delta and an inflationary token rate indicate the potential for lower rewards to be emitted in the future. An important factor that should help AVS developers determine the $AVS minting rate is that of rewarding operators for their capital costs.
             """)
 
 
@@ -3458,25 +3465,11 @@ def main():
     st.write("\n")
 
     st.markdown("""
-    **In-Isolation AVS3 Ratio:**
-
-    The Sharpe Ratio results represent the amount of net yield (after accounting for expected slashes) AVSs may earn going forward for each unit of risk, as measured by the standard deviation of those net yields over a previous time period.
-
-    The result of 20 represents the amount of net profit (after accounting for expected losses) earned for each unit of risk, as measured by the standard deviation of returns or profit.
+    **The Sharpe Ratio results represent the amount of net yield (after accounting for expected slashes) AVSs may earn going forward for each unit of risk, as measured by the standard deviation of those net yields over a previous time period.**
 
     The denominator of the Sharpe Ratio represents the standard deviation of the portfolio's excess returns, which is a measure of the investment's volatility or risk. Specifically, it quantifies how much the returns of the investment deviate from their average over a certain period. This measure is crucial in the context of the Sharpe Ratio because it provides a way to adjust for risk: by dividing the excess return (the return of the investment minus the risk-free rate) by the standard deviation of these excess returns, the Sharpe Ratio essentially tells you how much excess return you are getting for each unit of risk taken.
 
     In essence, the denominator of the Sharpe Ratio allows investors to understand the risk-adjusted return of an investment. A higher standard deviation indicates a higher level of risk (since the investment's returns are more spread out from the average), which in turn would require a higher excess return to achieve the same Sharpe Ratio. This helps investors compare investments on a risk-adjusted basis, making it easier to identify which investments are truly outperforming on a risk-adjusted basis rather than simply due to taking on more risk.
-
-    **AVS Reward Emission:**
-
-    The AVS Reward Emission simulator percentage-results suggest how much should be distributed among $AVS Stakers, xETH Restakers, and AVS Operators, given the risk and economic security profile of the AVS. To build on the Assumptions explanation on top, this calculation is based on the correlated reward-to-risk sensitivity per variable input -- the Logic below each input parameter helps understand the underlying rationale.
-
-    Operator Reward is naturally being given greater weight than the Staker Reward due to their paramount role in validating modules crucial to the well-function of an AVS. We've assigned 60% of distributable rewards to Operators and 40% to Stakers and Restakers.
-
-    The $AVS’s Tokenomics (while not included in the reward calculation) were deemed useful to include since they provide a look-ahead perspective of how the native AVS token can influence future rewards. A potential for improved rewards to be emitted in the future exists if a relatively small delta between circulating and total supply and a deflationary token rate exist. Whereas a larger delta and an inflationary token rate indicate the potential for lower rewards to be emitted in the future. An important factor that should help AVS developers determine the $AVS minting rate is that of rewarding operators for their capital costs.
-
-    It's important to bear in mind that this Simulator was built from an AVS perspective alone.
     """, unsafe_allow_html=False)
 
 
