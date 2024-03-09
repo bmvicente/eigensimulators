542981e0f17f3ef566d9e2b46e8c76f7789dbec0
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-09 02:17:15 +0100

    update

--- eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
+++ eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
@@ -3025,8 +3025,8 @@ def main():
             avs1_comp_loss_weight = st.slider("**AVS1 Compounded Loss/Actual Slash Weight**", min_value=10, max_value=90, value=50, format='%d%%', key="avs1_clac_w")
 
         with col94:
-            avs1_insurance_status_weight = 100 - avs1_net_yield_weight
-            st.slider("**AVS1 Insurance Status Weight**", min_value=10, max_value=90, value=avs1_expected_slash_weight, format='%d%%', disabled=True, key="avs1_is_w")
+            avs1_insurance_status_weight = 100 - avs1_comp_loss_weight
+            st.slider("**AVS1 Insurance Status Weight**", min_value=10, max_value=90, value=avs1_insurance_status_weight, format='%d%%', disabled=True, key="avs1_is_w")
         
         st.write("\n")
         st.write("\n")
