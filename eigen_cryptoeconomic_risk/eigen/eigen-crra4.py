6506618700c8ab48fdfa7f891505093663356c71
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-09 02:26:49 +0100

    update

--- eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
+++ eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
@@ -2982,6 +2982,7 @@ def main():
             st.write(f"""&#8226; AVS1 Standard Deviation: **${avs1_st_dev_abs:,.0f}**""")
 
         st.write("--------")
+        st.write("\n")
 
         st.write("**IN-ISOLATION WEIGHTINGS**")
 
