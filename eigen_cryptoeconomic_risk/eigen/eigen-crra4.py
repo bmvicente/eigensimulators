f07fdf89d80b56e71e70960cb814d789f5f15084
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-09 03:36:44 +0100

    update

--- eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
+++ eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
@@ -2976,7 +2976,7 @@ def main():
             <div style="
                 border: 2px solid;
                 border-radius: 5px;
-                padding: 10px;
+                padding: 8px;
                 text-align: center;
                 margin: 10px 0;
                 background-color: white;">
