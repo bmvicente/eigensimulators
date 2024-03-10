08966bb5e5de173530abac706594bf77c7fd3622
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-10 13:55:24 +0000

    update

--- eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
+++ eigen_cryptoeconomic_risk/eigen/eigen-crra4.py
@@ -4046,7 +4046,6 @@ def main():
         st.write("\n")
         st.write("\n")
         st.write("\n")
-        st.write("\n")
         
         agg_eco_sharpe_ratio = eco_sharpe_ratio1 + eco_sharpe_ratio2 + eco_sharpe_ratio3
 
