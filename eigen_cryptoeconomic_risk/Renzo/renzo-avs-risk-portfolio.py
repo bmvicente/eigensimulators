d53f0c887a611c697735c9a840e2dc596946ea66
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-30 00:31:48 +0000

    update

--- eigen_cryptoeconomic_risk/Renzo/renzo-avs-risk-portfolio.py
+++ eigen_cryptoeconomic_risk/Renzo/renzo-avs-risk-portfolio.py
@@ -113,10 +113,10 @@ def main():
     risk_score = renzo_avs_risk(avs_code_comp, avs_op_rep, avs_op_geo)
 
     # Determine the color and background color based on the risk score
-    if renzo_avs_risk >= 7.50:
+    if risk_score >= 7.50:
         renzo_color = "#d32f2f"  # Red color for high risk
         renzo_background_color = "#fde0dc"  # Light red background
-    elif renzo_avs_risk <= 2.50:
+    elif risk_score <= 2.50:
         renzo_color = "#388e3c"  # Green color for low risk
         renzo_background_color = "#ebf5eb"  # Light green background
     else:
