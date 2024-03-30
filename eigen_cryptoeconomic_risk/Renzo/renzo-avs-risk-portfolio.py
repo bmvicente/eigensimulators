c9f22109e84647be3d3a4eb999a9efc507885271
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-03-30 20:21:46 +0000

    update

--- eigen_cryptoeconomic_risk/Renzo/renzo-avs-risk-portfolio.py
+++ eigen_cryptoeconomic_risk/Renzo/renzo-avs-risk-portfolio.py
@@ -148,7 +148,7 @@ def main():
     <div style="
         border: 3px solid {renzo_color};
         border-radius: 5px;
-        padding: 10px;
+        padding: 8px;
         text-align: center;
         margin: 10px 0;
         background-color: {renzo_background_color};">
