df77f14388d645d12e0fbcac43a6b9de8b1fc061
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-04-30 15:28:38 +0300

    update

--- eigen_cryptoeconomic_risk/eigenavsrisk-omni.py
+++ eigen_cryptoeconomic_risk/eigenavsrisk-omni.py
@@ -503,7 +503,7 @@ def main():
             st.markdown("""
             <style>
             .header-style {
-                font-size: 15px;
+                font-size: 12px;
                 font-weight: bold;
                 margin-bottom: 0px;  /* Adjust the space below the header */
             }
