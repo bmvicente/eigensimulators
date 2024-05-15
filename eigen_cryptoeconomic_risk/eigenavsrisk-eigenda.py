44558b1cbe7dc23516bc5fe8b729a41a57f72ec4
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-05-15 16:06:35 +0300

    update

--- eigen_cryptoeconomic_risk/eigenavsrisk-eigenda.py
+++ eigen_cryptoeconomic_risk/eigenavsrisk-eigenda.py
@@ -1649,8 +1649,7 @@ Instead of requiring each node to download and store all data, EigenDA uses eras
     st.write("  \n")
     st.write("  \n")
     st.write("  \n")
-    st.write("  \n")
-    st.write("  \n")
+
 
     col81, col82, col83 = st.columns([5, 5, 1])
 
