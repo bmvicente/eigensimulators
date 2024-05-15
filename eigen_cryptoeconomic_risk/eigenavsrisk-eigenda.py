4fd5fc59426d77bdbc8f4fbd11053c2aa5e81fd7
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-05-15 15:33:44 +0300

    update

--- eigen_cryptoeconomic_risk/eigenavsrisk-eigenda.py
+++ eigen_cryptoeconomic_risk/eigenavsrisk-eigenda.py
@@ -1986,9 +1986,18 @@ Instead of requiring each node to download and store all data, EigenDA uses eras
 
 
         with col132:
-            st.write("**Liveness Violation**")
+            st.markdown(f"<div style='font-size: 21px; font-weight: bold;'>Liveness Violation</div>", unsafe_allow_html=True)
 
-            st.write("**Safety Violation**")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            st.write("")
+            
+            st.markdown(f"<div style='font-size: 21px; font-weight: bold;'>Safety Violation</div>", unsafe_allow_html=True)
 
 
 
