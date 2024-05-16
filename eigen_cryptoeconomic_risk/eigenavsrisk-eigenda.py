b02415dc2d6f523db123096cefc21b418649f3d1
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-05-16 11:35:15 +0300

    update

--- eigen_cryptoeconomic_risk/eigenavsrisk-eigenda.py
+++ eigen_cryptoeconomic_risk/eigenavsrisk-eigenda.py
@@ -2053,7 +2053,11 @@ Instead of requiring each node to download and store all data, EigenDA uses eras
     st.write("")
     st.write("")
     st.write("")
-
+    st.write("  \n")
+    st.write("  \n")
+    st.write("  \n")
+    st.write("  \n")
+    
     
     st.markdown(f"<div style='font-size: 26px; font-weight: bold;'>Game-Theoretic Dynamics Around Setting the KZG Erasure Encoding Rate by Rollup</div>", unsafe_allow_html=True)
 
