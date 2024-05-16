b372d9ffbd61c4ab45a552d93e139a9510374eed
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-05-16 17:31:35 +0300

    update

--- eigen_cryptoeconomic_risk/eigenavsrisk-eigenda.py
+++ eigen_cryptoeconomic_risk/eigenavsrisk-eigenda.py
@@ -1804,7 +1804,7 @@ Instead of requiring each node to download and store all data, EigenDA uses eras
 
     # Define min and max values for the final normalization based on the possible range of the final result
     min_final = 0  # Example value, adjust based on expected range
-    max_final = 1600 
+    max_final = 11153 
 
     # Normalize the final result
     normalized_risk_score = normalize_score(final_result, min_final, max_final)
