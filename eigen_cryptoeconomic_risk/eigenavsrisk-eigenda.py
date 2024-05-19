28e83cd5a403fb48513d1aadebb42c9efddc7b92
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-05-19 22:11:40 +0300

    update

--- eigen_cryptoeconomic_risk/eigenavsrisk-eigenda.py
+++ eigen_cryptoeconomic_risk/eigenavsrisk-eigenda.py
@@ -2097,7 +2097,7 @@ Instead of requiring each node to store all data, EigenDA uses erasure coding to
     st.markdown("""
     <style>
     .big-font {
-        font-size: 20px;  /* Adjust font size as needed */
+        font-size: 19px;  /* Adjust font size as needed */
     }
     </style>
     <div class="big-font">
