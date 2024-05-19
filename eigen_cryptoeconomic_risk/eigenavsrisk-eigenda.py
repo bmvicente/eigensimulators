4cc3420b9c1340629a474a6cb273b6491edc72e0
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-05-19 22:40:14 +0300

    update

--- eigen_cryptoeconomic_risk/eigenavsrisk-eigenda.py
+++ eigen_cryptoeconomic_risk/eigenavsrisk-eigenda.py
@@ -2053,24 +2053,35 @@ Instead of requiring each node to store all data, EigenDA uses erasure coding to
     st.markdown("""
     <style>
     .big-font {
+        font-size: 19px;  /* Adjust font size as needed */
+    }
+    </style>
+    <div class="big-font">
+        <strong>- Objectively Attributable Faults</strong> are straightforward to attribute since they are clearly verifiable onchain, making them suitable for direct slashing and redistribution of restaked ETH.
+        <br>
+        <strong>- Intersubjectively Attributable Faults</strong> require broad-based agreement among observers, and bEIGEN1-to-bEIGEN2 forking and bEIGEN1 burning to resolve disputes.
+        <br>
+        <strong>- Non-Attributable Faults</strong> cannot be attributed outside the victim of the fault. The inability to attribute these faults objectively or intersubjectively means that the funds involved are considered lost.
+    </div>
+    """, unsafe_allow_html=True)
+
+    st.write("")
+
+    st.markdown("""
+    <style>
+    .big-font {
         font-size: 21px;  /* Adjust font size as needed */
     }
     </style>
     <div class="big-font">
-        <strong>- Objectively Attributable Faults</strong> are straightforward to attribute since they are clearly verifiable onchain, making them suitable for direct slashing and redistribution of restaked ETH.</li>
-        <br>
-        <strong>- Intersubjectively Attributable Faults</strong> require broad-based agreement among observers, and bEIGEN1-to-bEIGEN2 forking and bEIGEN1 burning to resolve disputes.</li>
-        <br>
-        <strong>- Non-Attributable Faults</strong> cannot be attributed outside the victim of the fault. The inability to attribute these faults objectively or intersubjectively means that the funds involved are considered lost.</li>
-        <br>
-        <br>
         The Yellow-to-Red colouring on each category of Faults represent the increasing degree of risk.
         <br>
-        <strong>- Safety Violations</strong> normally require a 2/3 Stake Attack-type of malicious scenario, are objectively attributable, and are overall unlikely to occur to robust guardrails that are in place.
+        <strong>- Safety Violations</strong> normally require a 2/3 Stake Attack-type of malicious scenario, are objectively attributable, and are overall unlikely to occur due to robust guardrails that are in place.
         <br>
-        <strong>- Liveness Violations</strong> normally require a 1/3 Stake Attack-type of malicious scenario, are most pressing in DA due to the overt importance in reducing latency and throught in data storage attestation and signature registering. On top of that, since each risk vector can only be potentially solved through intersubjective faults, there is some leeway for malicious actors to try and corrupt the system, particularly through Collusion - which is non-attributable as a fault.
+        <strong>- Liveness Violations</strong> normally require a 1/3 Stake Attack-type of malicious scenario, are most pressing in DA due to the overt importance in reducing latency and throughput in data storage attestation and signature registering. On top of that, since each risk vector can only be potentially solved through intersubjective faults, there is some leeway for malicious actors to try and corrupt the system, particularly through Collusion - which is non-attributable as a fault.
     </div>
     """, unsafe_allow_html=True)
+
 
 
     st.write("")
