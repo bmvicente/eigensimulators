3961e3fdfbd2d3cd5aa35b6a3070e84afed507e4
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-05-19 14:20:49 +0300

    update

--- eigen_cryptoeconomic_risk/eigenavsrisk-eigenda.py
+++ eigen_cryptoeconomic_risk/eigenavsrisk-eigenda.py
@@ -2063,11 +2063,12 @@ Instead of requiring each node to store all data, EigenDA uses erasure coding to
             <li><strong>Non-Attributable Faults</strong> cannot be attributed outside the victim of the fault. The inability to attribute these faults objectively or intersubjectively means that the funds involved are considered lost.</li>
         </ul>
         <br>
+        <br>
         The Yellow-to-Red colouring on each category of faults represent the increasing risk in that order.
         <br>
-        <strong>Safety Violations</strong> normally require a 2/3 Stake Attack-type of malicious scenario, are objectively attributable and are overall unlikely to occur to robust guardrails that are in place.
+        - <strong>Safety Violations</strong> normally require a 2/3 Stake Attack-type of malicious scenario, are objectively attributable and are overall unlikely to occur to robust guardrails that are in place.
         <br>
-        <strong>Liveness Violations</strong> are most pressing in DA due to the overt importance in reducing latency and throught in data storage attestation and signature registering. On top of that, since each risk vector can only be potentially solved through intersubjective faults, there is some leeway for malicious actors to try and corrupt the system, particularly through Collusion.
+        - <strong>Liveness Violations</strong> are most pressing in DA due to the overt importance in reducing latency and throught in data storage attestation and signature registering. On top of that, since each risk vector can only be potentially solved through intersubjective faults, there is some leeway for malicious actors to try and corrupt the system, particularly through Collusion.
     </div>
     """, unsafe_allow_html=True)
 
