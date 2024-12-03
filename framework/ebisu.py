d0d9fbe2f6bd5ce049df1801b3f0002f14dd005d
Bernardo Vicente <bernardov.vicente@gmail.com>
2024-12-03 16:21:10 +0100

    update

--- framework/ebisu.py
+++ framework/ebisu.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-st.set_page_config(page_title="Ebisu Dashboard", layout="wide")
+st.set_page_config(page_title="Ebisu LRT Risk Dashboard", layout="wide")
 st.image("framework/images/ebisunoback.png", width=150)
 st.title("Ebisu Dashboard")
 
