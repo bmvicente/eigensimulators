4d451fe8040a2ef7ec1de51ef552a74f73ba339b
Bernardo Vicente <bernardov.vicente@gmail.com>
2025-01-16 10:33:08 +0100

    update

--- framework/ebisu-liquidity.py
+++ framework/ebisu-liquidity.py
@@ -907,7 +907,7 @@ In terms of tokenomics, LBTC's integration into DeFi introduces new dynamics for
 
 
             if token == "weETH":
-                num_spaces = 17
+                num_spaces = 13
             elif token == "sUSDe":
                 num_spaces = 16
             elif token == "LBTC":
